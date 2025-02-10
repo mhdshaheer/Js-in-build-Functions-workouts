ea73edf4ae22781d42c9061c56a608d01493f088
Shaheer cp <shaheercp890@gmail.com>
2025-02-10 15:56:28 +0530

    Array method : findIndex()

--- array.js
+++ array.js
@@ -15,5 +15,29 @@
 // splice() – Modifies an array by removing or replacing elements.
 // sort() – Sorts an array in place.
 // reverse() – Reverses the elements of an array
+// fill() 
 
-console.log("hai");
+//============================= findIndex() ======================================
+// findIndex() – Returns the index of the first element that meets a condition.
+// if no element found return '-1'.
+
+// #1
+// const arr = [1,3,7,2,8];
+// let index = arr.findIndex(item=>item%2==0)
+// console.log(index) // It will return the index of that condition satisfied first element.
+
+// #2
+// const obj = [
+//     {name:'shahan',age:22},
+//     {name:'shaheer',age:22},
+//     {name:'arjun',age:21}
+// ];
+// const index = obj.findIndex(item=>item.name==='shaheer');
+// console.log(index)
+
+// #3
+// const nums = [5,8,4,3,2];
+// const index = nums.findIndex(item=>item>10);
+// console.log(index); // Here no element satisfies the condition so they return '-1'.
+
+//================================================================================
